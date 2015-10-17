25e4f0dd41f4d66f8e82a1edf0e06c7282bf86c9
Michael Baumgärtner <mib@yamanote.de>
2015-10-17 15:44:58 +0200

    index create command

--- source/galleries_script.py
+++ source/galleries_script.py
@@ -3,14 +3,24 @@
 
 import sys, os
 
+from wheezy import template as wheezy
+
+template_engine = wheezy.engine.Engine(
+  loader=wheezy.loader.FileLoader(['/home/dreizehn/scripts/source/templates']),
+  extensions=[wheezy.ext.core.CoreExtension()]
+)
+
 from galleries.galleries import search_galleries
 
 def galleries_list(args):
   galleries = search_galleries(args.path)
   for gallery in galleries:
-    print gallery
-    for album in gallery.albums:
-      print "", album
+    print("{0:40} | {1}".format(gallery, ", ".join(str(x) for x in gallery.albums)))
+
+def index_create(args):
+  galleries = search_galleries(args.path)
+  template = template_engine.get_template('galleries_index.html')
+  print(template.render({'galleries': galleries}))
 
 DESCRIPTION = """
 This script handles content and access rights for web galleries.
@@ -36,6 +46,15 @@ def main(args=None):
   parser_list = subparsers.add_parser('list',
     help='List all galleries.')
   parser_list.set_defaults(func=galleries_list)
+
+  # index command
+  parser_index = subparsers.add_parser('index',
+    help='Manage index html.')
+  subparsers_index = parser_index.add_subparsers(title='index commands')
+  # index create command
+  parser_index_create = subparsers_index.add_parser('create',
+    help='Create index html.')
+  parser_index_create.set_defaults(func=index_create)
 
   arguments = parser.parse_args() if args == None else parser.parse_args(args)
   arguments.func(arguments)
